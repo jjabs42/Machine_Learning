94fa8d8b875f16c68e42f8f72e7a6b42d1707585
jjabs <jtjabero@icloud.com>
2026-07-02 19:19:13 -0400

    finished the basic gradient descent logic

---
+++ week1/project_linear_regression/gd_1Var_simple.py
@@ -0,0 +1,68 @@
+#simple linear regression using gradient descent - not using numpy calculations
+import time
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+#configs:
+learning_rate = 0.0005
+
+df = pd.read_csv("data.csv")
+
+x_array = pd.Series.to_numpy(df["x"])
+y_array = pd.Series.to_numpy(df["y"])
+#returns the partial derivitives d/dw and d/db for the cost function, at points w and b
+def derivitive(x, y, w, b) -> tuple:
+    
+    shape = x.shape[0]
+
+    #finds w and b
+    der_w = 0
+    der_b = 0
+    for i in range(shape):
+        der_w+=(w*float(x[i])+b-float(y[i]))*float(x[i])
+        der_b+=(w*float(x[i])+b-float(y[i]))
+    der_w/=shape
+    der_b/=shape
+
+    return(float(der_w), float(der_b))
+
+
+#takes in the parameters and returns the tuple of the ideal w, b
+def gradient_descent(x, y, w, b, learning_rate) -> tuple:
+    while True:
+        print(w)
+        dw, db = derivitive(x, y, w, b)
+        w_old = w
+        w -=learning_rate*dw
+        b -=learning_rate*db
+        print(abs(w_old-w))
+        if abs(w_old - w) < 1e-8:
+            return w, b
+            
+            
+    
+def cost(x, y, w, b):
+    shape = x.shape[0]
+    sum = 0
+    for i in range(shape):
+        sum += (w*x[i] + b - y[i])**2
+    return sum / (2*shape)
+
+w, b = gradient_descent(x_array, y_array, 0, 0, learning_rate)
+
+
+
+plt.plot(x_array, y_array, "ro")
+plt.axline((0,b), slope=w)
+plt.show()
+
+
+
+
+
+
+
+
+
+
---
+++ week1/project_linear_regression/test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+x = np.array([3,4,5])
+print(type(float(x[1])))
